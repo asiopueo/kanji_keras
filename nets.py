004992af0688895cdbb20022b8c188018294d6af
Martin <martin.lippl@pueo.de>
2019-06-24 00:52:31 +0200

    Final working version of model M6-2

--- nets.py
+++ nets.py
@@ -9,10 +9,10 @@ import numpy as np
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
-from keras.layers import MaxPooling2D, Flatten
+from keras.layers import MaxPooling2D, Flatten, BatchNormalization
 from keras.layers.convolutional import Convolution2D
 from keras.utils import np_utils, to_categorical
-
+from keras.optimizers import Adam
 
 
 
@@ -67,24 +67,39 @@ def model_M6_2():
 	model = Sequential()	
 
 	# input_shape=(1,64,64)
-	model.add( Convolution2D(64, (3,3), input_shape=(1,64,64), padding='valid', strides=(1,1), activation='relu', data_format='channels_first') ) # conv3-64
+	model.add( Convolution2D(64, (3,3), input_shape=(1,64,64), padding='valid', strides=(1,1), kernel_initializer='normal', activation='relu', data_format='channels_first') ) # conv3-64
+	model.add( BatchNormalization() )
+	#model.add( Activation('relu') ) 
 	model.add( MaxPooling2D(pool_size=(2,2), data_format='channels_first') )
+	model.add( Dropout(0.5) )
 
 	# shape = ()
-	model.add( Convolution2D(128, (3,3), padding='valid', activation='relu', data_format='channels_first') ) # conv3-128
+	model.add( Convolution2D(128, (3,3), padding='valid', kernel_initializer='normal', activation='relu', data_format='channels_first') ) # conv3-128
+	model.add( BatchNormalization() )
+	#model.add( Activation('relu') )
 	model.add( MaxPooling2D(pool_size=(2,2), data_format='channels_first') )
-
+	model.add( Dropout(0.5) )
+	
 	# shape = ()
-	model.add( Convolution2D(512, (3,3), padding='valid', activation='relu', data_format='channels_first') ) # conv3-512
+	model.add( Convolution2D(512, (3,3), padding='valid', kernel_initializer='normal', activation='relu', data_format='channels_first') ) # conv3-512
+	model.add( BatchNormalization() )
+	#model.add( Activation('relu') )
 	model.add( MaxPooling2D(pool_size=(2,2), data_format='channels_first') )
+	model.add( Dropout(0.5) )
 
 	model.add( Flatten() )
-	model.add( Dense(4096) )
-	model.add( Dense(4096) )
-	model.add( Dense(320) )
+	model.add( Dense(4096, kernel_initializer='random_uniform') )
+	model.add( Dropout(0.5) )
+	model.add( BatchNormalization() )
+	model.add( Dense(4096, kernel_initializer='random_uniform') )
+	model.add( Dropout(0.5) )
+	model.add( BatchNormalization() )
+	model.add( Dense(320, kernel_initializer='random_uniform') )
+	model.add( BatchNormalization() )
 	model.add( Activation('softmax') )
 	
-	model.compile(loss='categorical_crossentropy', optimizer='adam')
+	adam = Adam(lr=1e-2, beta_1=0.9, beta_2=0.999, epsilon=1e-8, decay=0.1, amsgrad=False)
+	model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
 	return model
 
 
@@ -110,6 +125,7 @@ def model_M7_1():
 	model.compile(loss='categorical_crossentropy', optimizer='adam')
 	return model
 
+
 # Model M7-2
 def model_M7_2():
 	model = Sequential()
@@ -133,27 +149,46 @@ def model_M7_2():
 
 	return model
 
+
 # Model M8
 def model_M8():
 	model = Sequential()
 	model.add( Convolution2D(32, (3,3), input_shape=(1,64,64), padding='valid', strides=(1,1), activation='relu', data_format='channels_first') )
+	model.add( Dropout(0.5) )
+	model.add( BatchNormalization() )
 	model.add( Convolution2D(32, (3,3), padding='valid', strides=(1,1), activation='relu', data_format='channels_first') )
+	model.add( Dropout(0.5) )
+	model.add( BatchNormalization() )
 	model.add( MaxPooling2D(pool_size=(2,2), data_format='channels_first') )
 
 	model.add( Convolution2D(64, (3,3), padding='valid', strides=(1,1), activation='relu', data_format='channels_first') )
+	model.add( Dropout(0.5) )
+	model.add( BatchNormalization() )
 	model.add( Convolution2D(64, (3,3), padding='valid', strides=(1,1), activation='relu', data_format='channels_first') )
+	model.add( Dropout(0.5) )
+	model.add( BatchNormalization() )
 	model.add( MaxPooling2D(pool_size=(2,2), data_format='channels_first') )
 
 	model.add( Convolution2D(128, (3,3), padding='valid', strides=(1,1), activation='relu', data_format='channels_first') )
+	model.add( Dropout(0.5) )
+	model.add( BatchNormalization() )
 	model.add( Convolution2D(128, (3,3), padding='valid', strides=(1,1), activation='relu', data_format='channels_first') )
+	model.add( Dropout(0.5) )
+	model.add( BatchNormalization() )
 	model.add( MaxPooling2D(pool_size=(2,2), data_format='channels_first') )
 
 	model.add( Flatten() )
 	model.add( Dense(1024) )
+	model.add( Dropout(0.5) )
+	model.add( BatchNormalization() )
 	model.add( Dense(320) )
+	model.add( BatchNormalization() )
 	model.add( Activation('softmax') )
 
+	adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-4, decay=0.1, amsgrad=False)
+	model.compile(loss='categorical_crossentropy', optimizer=adam)
 	return model
+
 
 # Model M9
 def model_M9():
